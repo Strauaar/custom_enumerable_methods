d5254206a4cee237ce710e0b20de7541c48e9247
Aaron <astrauli@gmail.com>
2017-04-13 18:34:33 -0700

    Add full feature self-made Enumberable inject method

--- enMethods.rb
+++ enMethods.rb
@@ -107,8 +107,58 @@ module Enumerable
       return selfArr.length
     end
   end
+
+
+#puts [1,2,4,5,6,4].my_count(4) #=> 2
+#puts [1,2,4,5,6,4].my_count #=> 6
+#puts [1,2,4,5,6,4].my_count {|x| x > 2} #=> 4
+
+
+  def my_map
+    selfArr = self.to_a
+    newArr = []
+    for i in 0...selfArr.length
+      if block_given?
+        newArr.push(yield(selfArr[i]))
+      else
+        newArr.push(selfArr[i])
+      end
+    end
+    return newArr
+  end
+
+#print (1..3).my_map #=> [1,2,3]
+#print [1,2,3].my_map {|x| x * 2} #=> [2,4,6]
+
+  def my_inject(initial = nil, sym = nil)
+      selfArr = self.to_a
+      cumulative = 0
+      if initial
+        selfArr.insert(0, initial)
+      else
+      end
+      for i in 0...selfArr.length
+        if block_given?
+          if i == 0
+          elsif i == 1
+            toAccum = yield(selfArr[i-1],selfArr[i])
+          else
+            toAccum = yield(toAccum,selfArr[i])
+          end
+        else
+          if i == 0
+          elsif i == 1
+            toAccum = sym.send(selfArr[i-1],selfArr[i])
+          else
+            toAccum = sym.send(toAccum,selfArr[i])
+          end
+        end
+      end
+  end
 end
 
-puts [1,2,4,5,6,4].my_count(4) #=> 2
-puts [1,2,4,5,6,4].my_count #=> 6
-puts [1,2,4,5,6,4].my_count {|x| x > 2} #=> 4
+a= [1,2,3]
+puts a.inject(:+) # => 6
+puts a.inject(2, :+) # =>8
+puts a.inject(2) {|cumulative, item| cumulative*item} # =>12
+puts a.inject{|cumulative,item| cumulative*item} # => 6
